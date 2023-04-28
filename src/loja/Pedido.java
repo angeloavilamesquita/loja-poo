667f3ba2fa43c3d32e9c646f18ab85b07f8bc56a
Angelo Mesquita <angelo.mesquita@professor.unis.edu.br>
2023-04-27 21:51:46 -0300

    commit inicial

---
+++ src/loja/Pedido.java
@@ -0,0 +1,58 @@
+package loja;
+
+import java.util.ArrayList;
+
+public class Pedido {
+    private String dataDeCriacao;
+    private String status;
+    private Cliente cliente;
+    private ArrayList<PedidoItem> itens = new ArrayList<>();
+    
+    public void addItem(PedidoItem item) {
+        this.itens.add(item);
+    }
+    
+    public PedidoItem removeItem(PedidoItem item) {
+        if (this.itens.contains(item)) {
+            this.itens.remove(item);
+        }
+        return item;
+    }
+    
+    public double calculaValorTotal() {
+        double total = 0;
+        for (PedidoItem item : itens) {
+            total += item.getSubtotal();
+        }
+        return total;
+    }
+
+    public String getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(String dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<PedidoItem> getItens() {
+        return itens;
+    }
+    
+}
